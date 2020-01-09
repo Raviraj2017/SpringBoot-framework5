f143e1549517b7866a149e45e92aa9d61fe4741f
raviraj2017 <Ravirajtiwari20@gmail.com>
2020-01-09 23:18:04 +0530

    adding JPA Example

---
+++ SpringFramwork5/src/main/java/com/springfeamework5/SpringFramwork5/SpringFramwork5Application.java
@@ -0,0 +1,13 @@
+package com.springfeamework5.SpringFramwork5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramwork5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFramwork5Application.class, args);
+	}
+
+}
